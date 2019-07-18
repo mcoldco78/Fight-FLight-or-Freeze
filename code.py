ed964c2d3cc4d30e9cbfeeb17307247025d0ead9
mcoldco78 <51186043+mcoldco78@users.noreply.github.com>
2019-07-17 22:56:53 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,44 @@
-print('hello')
+import random
+import time
+
+def scene1():
+  print("It's 1 am, you're laying in bed drifting off to sleep when you notice your lamp went out")
+  time.sleep(5)
+  print("You get up to investigate, as you know that light bulb is new")
+  time.sleep(5)
+  print("You go to the hallway to test out that light, process of elimanation. It does not work..")
+  time.sleep(7)
+  print("Out of furthered curousity, you go down the hallways to test another room.")
+  time.sleep(8)
+  print("You make it to the end of the hallway when you realize that the fron-tdoor at the bottom of the steps is wide open")
+  
+  def choosePath():
+      path = ""
+      while path != "1" and path != "2":
+        path = input("What do you do know? (1 or 2) : ")
+        
+        return path 
+      
+  def checkPath(chosenPath) :
+      print("Will bolting to the door be the smartest path?")
+      time.sleep(6)
+      print("Or does hiding in the bedroom closet make sense?")
+      
+      correctPath = random.randint(1, 2)
+      
+      if chosenPath == str(correctPath) :
+          print("You succusfully ran out the door")
+      else:
+          print("You hesitate, you're grabbed by a set of hands coming from the bathroom to your back")
+  
+  playagain = "yes"
+  while playAgain == "yes" or playAgain == "y":
+  scene1()
+  choice = choosePath()
+  checkPath(choice)
+  playagain = input ("Do you want to play again? (yes or y to continue playing) : ") 
+       
+  
 
 
 
